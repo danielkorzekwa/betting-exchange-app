9dfc53b16c5ce31d71ba22b284a8b833041da690
daniel.korzekwa <daniel.korzekwa@8c1298ea-1601-33d9-4d92-df0acfbcf972>
2011-06-20 20:10:34 +0000

    Issue 3:	Improve feeding performance of market data

--- src/test/scala/dk/betex/server/BetexResourceTest.scala
+++ src/test/scala/dk/betex/server/BetexResourceTest.scala
@@ -4,8 +4,10 @@ import com.sun.jersey.test.framework.JerseyTest
 import org.junit._
 import org.junit.Assert._
 import com.sun.jersey.api.client.WebResource
+import org.codehaus.jettison.json._
 
-/**http://10.2.4.191/createMarket?marketId=10&marketName=m1&eventName=e1&numOfWinners=1&marketTime=10&runners=11:r1,12:r2
+/**
+ * http://10.2.4.191/createMarket?marketId=10&marketName=m1&eventName=e1&numOfWinners=1&marketTime=10&runners=11:r1,12:r2
  * http://10.2.4.191/placeBet?betId=4&betSize=3&betPrice=2&betType=LAY&marketId=10&runnerId=11
  */
 class BetexResourceTest extends JerseyTest("dk.betex.server") {
@@ -62,7 +64,7 @@ class BetexResourceTest extends JerseyTest("dk.betex.server") {
     assertEquals("""{"markets":[]}""", markets)
   }
 
-  /**Test scenarios for placeBet.*/
+  /**Test scenarios for placeBet*/
   @Test
   def place_back_bet() {
     createMarket(1)
@@ -241,6 +243,52 @@ class BetexResourceTest extends JerseyTest("dk.betex.server") {
 
     val bestPricesMsg = webResource.path("/getBestPrices").queryParam("marketId", "123").get(classOf[String])
     assertEquals("""{"marketPrices":[{"runnerId":11},{"runnerId":12,"bestToBackPrice":2.2,"bestToBackTotal":20}]}""", bestPricesMsg)
+  }
+
+  /**Test scenarios for process Betex events*/
+  @Test
+  def process_betex_events {
+
+    val marketEvents = new JSONArray()
+    marketEvents.put(new JSONObject("""{"time":1234567,"eventType":"CREATE_MARKET",
+				"marketId":1, 
+				"marketName":"Match Odds",
+				"eventName":"Man Utd vs Arsenal", 
+				"numOfWinners":1, 
+				"marketTime":
+				"2010-04-15 14:00:00", 
+				"runners": [{"runnerId":11,
+				"runnerName":"Man Utd"},
+				{"runnerId":12, 
+				"runnerName":"Arsenal"}]
+				}"""))
+
+    marketEvents.put(new JSONObject("""{"time":1234568,"eventType":"PLACE_BET",	
+				"betSize":10,
+				"betPrice":3,
+				"betType":"LAY",
+				"marketId":1,
+				"runnerId":11
+				} """))
+				
+				 marketEvents.put(new JSONObject("""{"time":1234568,"eventType":"PLACE_BET",	
+				"betSize":20,
+				"betPrice":3.1,
+				"betType":"BACK",
+				"marketId":1,
+				"runnerId":11
+				} """))
+
+    marketEvents.put(new JSONObject("""{"time":12345611,"eventType":"CANCEL_BETS","betsSize":3.0,"betPrice":3,"betType":"LAY","marketId":1,"runnerId":11}"""))
+
+    val marketEventsData = new JSONObject()
+    marketEventsData.put("userId", 123)
+    marketEventsData.put("marketEvents", marketEvents)
+    val webResource = resource().path("/processBetexEvents").`type`("application/json").post(classOf[String], marketEventsData.toString)
+    assertEquals("OK", webResource)
+
+    val bestPricesMsg = resource().path("/getBestPrices").queryParam("marketId", "1").get(classOf[String])
+    assertEquals("""{"marketPrices":[{"runnerId":11,"bestToBackPrice":3,"bestToBackTotal":7,"bestToLayPrice":3.1,"bestToLayTotal":20},{"runnerId":12}]}""", bestPricesMsg)
   }
 
   private def createMarket(marketId: Long): String = {
